99b799752d95d8791b200b50aa56cca6b0c19860
gabrial.gsm@gmail.com <gabrial.gsm@gmail.com>
2019-02-18 14:51:08 +0600

    segment code added to footer

--- footer.php
+++ footer.php
@@ -76,5 +76,12 @@
 	</footer>
 
 	<?php wp_footer(); ?>
+
+	<script type="text/javascript">
+		!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+		analytics.load("eT8UicDX9RjxoK0UDpFpdnPCaOm1eXK0");
+		analytics.page();
+		}}();
+	</script>
 </body>
 </html>
